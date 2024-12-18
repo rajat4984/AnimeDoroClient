d8cb8dcf4a83cfe84a2614c9bcb4c9d1fed7580d
rajat4984 <rs20021023@gmail.com>
2024-12-19 00:33:26 +0530

    circular progress fixed to dynamic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import '../styles/components/timer.scss';
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
-import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-import { addPomoData } from '../redux/chartSlice/chartSlice';
-import { useCookies } from 'react-cookie';
+import { useEffect, useState } from "react";
+import "../styles/components/timer.scss";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { addPomoData } from "../redux/chartSlice/chartSlice";
+import { useCookies } from "react-cookie";
 const Timer = () => {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const userState = useSelector((store) => store.user);
@@ -12,7 +12,7 @@ const Timer = () => {
 
   const dispatch = useDispatch();
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 468px)');
+    const mediaQuery = window.matchMedia("(max-width: 468px)");
     const handleMediaQueryChange = (event) => {
       setIsSmallScreen(event.matches);
     };
@@ -37,29 +37,29 @@ const Timer = () => {
 
   const playAudio = (audioName) => {
     let audio = new Audio(audioName);
-    if (music === 'On') audio.play();
+    if (music === "On") audio.play();
   };
   return (
     <div className="timer">
       <CountdownCircleTimer
         isPlaying={isPlaying}
-        duration={10}
+        duration={pomoTime * 60}
         // duration={5}
-        colors={' #f35757'}
-        strokeLinecap={'round'}
+        colors={" #f35757"}
+        strokeLinecap={"round"}
         strokeWidth={6}
         size={isSmallScreen ? 180 : 250}
-        trailColor={'#ffcccc'}
+        trailColor={"#ffcccc"}
         onComplete={() => {
           setIsPlaying(false);
-          playAudio('/assets/sounds/bell.wav');
+          playAudio("/assets/sounds/bell.wav");
           const paramsObj = {
             userId: userState.user.userId,
             minutes: pomoTime,
-            token:cookies.access_token
+            token: cookies.access_token,
           };
 
-          console.log(paramsObj,'paramsObj');
+          console.log(paramsObj, "paramsObj");
 
           dispatch(addPomoData(paramsObj));
         }}
@@ -73,7 +73,7 @@ const Timer = () => {
             <FaPauseCircle
               size={30}
               onClick={() => {
-                playAudio('/assets/sounds/btn-click.wav');
+                playAudio("/assets/sounds/btn-click.wav");
                 setIsPlaying(false);
               }}
             />
@@ -81,7 +81,7 @@ const Timer = () => {
             <FaPlayCircle
               size={30}
               onClick={() => {
-                playAudio('/assets/sounds/btn-click.wav');
+                playAudio("/assets/sounds/btn-click.wav");
                 setIsPlaying(true);
               }}
             />
